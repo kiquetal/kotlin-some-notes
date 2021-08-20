946f7b855332a2cc560ce2f14ad8ac71ee368f36
Enrique M. Talavera <kiquetal@gmail.com>
2021-08-19 23:56:06 -0400

    formatting text

--- codes/src/main/kotlin/chapter9part2.kt
+++ codes/src/main/kotlin/chapter9part2.kt
@@ -1,49 +1,50 @@
-package chapter9.part2;
+package chapter9.part2
+
 interface Dialable {
-    fun dial():Unit
+    fun dial(): Unit
 }
+
 interface Snapable {
 
-    fun snap():Unit
+    fun snap(): Unit
 }
 
-class IPhone: Dialable {
+class IPhone : Dialable {
     override fun dial() {
-        println("some iphone dial");
+        println("some iphone dial")
     }
 
 }
 
 class Phone : Dialable {
     override fun dial() {
-        println("some generic dial");
+        println("some generic dial")
     }
 
 }
+
 class Camera : Snapable {
     override fun snap() {
-        println("some snap");
+        println("some snap")
     }
 
 }
 
-class Smartphone (
-    private val phone: Dialable ,
-    private val camera:Snapable
-): Dialable by phone,Snapable by camera {
-    override fun dial()  {
-          if (phone is IPhone)
-        {
+class Smartphone(
+    private val phone: Dialable,
+    private val camera: Snapable
+) : Dialable by phone, Snapable by camera {
+    override fun dial() {
+        if (phone is IPhone) {
             println("no iphone!!! allowed")
         }
-          }
+    }
 
 
 }
 
 
-
-fun main(args:Array<String>) {
-    val m = Smartphone(IPhone(),Camera());
+fun main(args: Array<String>) {
+    val m = Smartphone(IPhone(), Camera())
     println(m.dial())
 }
